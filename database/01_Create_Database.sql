3b57feea5528e84a37ce56f01dcdae3163b2ce4d
Bismarck <sam@aepearce.com>
2022-01-13 18:44:53 +0100

    =Uploaded my progress so far

---
+++ database/01_Create_Database.sql
@@ -0,0 +1,6 @@
+###################################
+# Sporia Database Creation Script #
+###################################
+
+SELECT 'Have you got the latest version?' AS 'Script\'s still being developed buckaroo';
+EXIT;
